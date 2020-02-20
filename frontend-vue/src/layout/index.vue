7c835ca6d309ec638fa84b3ef9879cfa8fc65416
Ajay M <ajay@acid-tech.com>
2020-02-20 15:47:54 +0530

    Removed right side bar

--- frontend-vue/src/layout/index.vue
+++ frontend-vue/src/layout/index.vue
@@ -2,21 +2,17 @@
   <div :class="classObj" class="app-wrapper">
     <div v-if="device==='mobile'&&sidebar.opened" class="drawer-bg" @click="handleClickOutside" />
     <sidebar class="sidebar-container" />
-    <div :class="{hasTagsView:needTagsView}" class="main-container">
+    <div class="main-container">
       <div :class="{'fixed-header':fixedHeader}">
         <navbar />
       </div>
       <app-main />
-      <right-panel v-if="showSettings">
-        <settings />
-      </right-panel>
     </div>
   </div>
 </template>
 
 <script>
-import RightPanel from "@/components/RightPanel";
-import { AppMain, Navbar, Settings, Sidebar } from "./components";
+import { AppMain, Navbar, Sidebar } from "./components";
 import ResizeMixin from "./mixin/ResizeHandler";
 import { mapState } from "vuex";
 
@@ -25,18 +21,13 @@ export default {
   components: {
     AppMain,
     Navbar,
-    RightPanel,
-    Settings,
     Sidebar
   },
   mixins: [ResizeMixin],
   computed: {
     ...mapState({
       sidebar: state => state.app.sidebar,
-      device: state => state.app.device,
-      showSettings: state => state.settings.showSettings,
-      needTagsView: state => state.settings.tagsView,
-      fixedHeader: state => state.settings.fixedHeader
+      device: state => state.app.device
     }),
     classObj() {
       return {
