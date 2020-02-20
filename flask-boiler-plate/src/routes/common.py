babe5f33133c744557d3bab3c27741db37e23799
Ajay M <ajay@acid-tech.com>
2020-02-20 19:40:12 +0530

    Added user's routes

--- flask-boiler-plate/src/routes/common.py
+++ flask-boiler-plate/src/routes/common.py
@@ -7,10 +7,55 @@ from utils import return_json, getVPNServersList
 from __main__ import app
 
 
-@app.route("/vpn-servers", methods=["GET"])
-def getVPNServers():
-    data = getVPNServersList()
-    return return_json(data)
+@app.route("/regions", methods=["GET"])
+def getRegions():
+    data = [
+        {
+            "server": "192.168.137.236",
+            "region": "Local"
+        },
+        {
+            "server": "192.168.137.23",
+            "region": "Local"
+        },
+        {
+            "server": "192.168.137.22",
+            "region": "Local"
+        },
+        {
+            "server": "192.168.137.256",
+            "region": "Local"
+        },
+        {
+            "server": "192.168.137.2",
+            "region": "Local"
+        }
+    ]
+    response = {
+        "code": 20000,
+        "data": data
+    }
+    return return_json(response)
+
+@app.route("/facebook-users", methods=["GET"])
+def getFacebookUsers():
+    data = [
+        {
+            "username": "kalakaar.humara",
+            "password": "Asafaf",
+            "status": 1,
+            "region": "192.168.137.236",
+            "total_scraps": 10,
+            "last_checked": 1578523941
+        }
+    ]
+    response = {
+        "data": data,
+        "code": 20000
+    }
+    return return_json(response)
+
+
 
 @app.route("/link-types", methods=["GET"])
 def getFacebookLinkTypes():
@@ -27,3 +72,4 @@ def getFacebookLinkTypes():
         "code": 20000
     }
     return return_json(response)
+
